5b53b506526c3f7d32bb3975e8310910c1cce215
Deniiik <denisruzin425@gmail.com>
2024-10-31 19:25:40 +0300

    Update Task_1.py

--- Лабораторная работа 2/Task_1.py
+++ Лабораторная работа 2/Task_1.py
@@ -2,8 +2,8 @@ money_capital = 20000  # Подушка безопасности
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
-count_months = 1
-while money_capital > 0 and spend < money_capital:
+count_months = 0
+while money_capital > spend - salary:
     money_capital += salary - spend
     spend = spend * (1 + increase)
     count_months += 1
