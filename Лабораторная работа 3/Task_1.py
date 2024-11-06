bcad2a705a97c7ccbd7aa489a9901cc41da79c58
Deniiik <denisruzin425@gmail.com>
2024-11-06 03:44:57 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/Task_1.py
@@ -0,0 +1,16 @@
+# TODO Напишите функцию для поиска индекса товара
+def search_index(items, i):
+    if i in items:
+        return items.index(i)
+    else:
+        return None
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = search_index(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
---
+++ Лабораторная работа 3/Task_2.py
@@ -0,0 +1,11 @@
+def find_common_participants(first_group, second_group, comma=','):
+    group_one = first_group.split(comma)
+    group_two = second_group.split(comma)
+    common_participants = list(set(group_one).intersection(group_two))
+    return sorted(common_participants)
+
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+print("Общие участники:", find_common_participants(participants_first_group, participants_second_group, "/"))
